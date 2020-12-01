ec4e64e8d2f9d57a140426c61b4e868e9024cd3e
Sasha-M-Motielall <smotielall20@stuy.edu>
2020-12-01 13:42:53 -0500

    equals method tranfered

--- RealNumber.java
+++ RealNumber.java
@@ -13,18 +13,6 @@ public class RealNumber{
     return ""+getValue();
   }
 //---------------------------------------
-  public boolean equals(RealNumber other){
-    if(this.getValue() == 0){
-      return other.getValue() == 0;
-    }
-    else if(Math.abs(this.getValue() - other.getValue())/((this.getValue() + other.getValue())/2) * 100 <= 0.001){
-      return true;
-    }
-    else{
-      return false;
-    }
-  }
-
   public RealNumber add(RealNumber other){
      RealNumber sum = new RealNumber(getValue() + other.getValue());
      return sum;
